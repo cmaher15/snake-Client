a5d03930b34fa025aec8da59f5c6e0293ce7aac7
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-11 14:05:45 -0700

    created server connection message as well as a name for the snek

--- client.js
+++ client.js
@@ -5,7 +5,15 @@ const connect = function() {
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: 50541
-  });
+  }
+  );
+
+  //message when player successfully connects to the server 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server!');
+    conn.write('Name: CIM');
+  })
+
 
   //message when player is kicked out for idling
   conn.on('data', (data) => {
