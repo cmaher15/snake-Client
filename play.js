22a782b8a0220f9d75fcb2506b168d30559a0aa8
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-11 15:01:14 -0700

    exported input into play and moved the user and setup functions into the input file

--- play.js
+++ play.js
@@ -1,22 +1,9 @@
 const connect = require("./client");
+const setUpInput = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+setUpInput();
 
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-setupInput();
