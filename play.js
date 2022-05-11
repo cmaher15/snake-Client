d660f004da382526017eb92b0b947b9b22660e64
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-11 13:45:03 -0700

    exported the connnect function from play.js to client.js. Then exported it in the client file and required it in the play file.

--- play.js
+++ play.js
@@ -1,23 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
-
-  //message when player is kicked out for idling
-  conn.on('data', (data) => {
-    console.log('you ded cuz you idled');
-    conn.end();
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const connect = require("./client");
 
 console.log("Connecting ...");
 connect();
