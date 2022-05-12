55511998037c0fa59d1ecb039338f2b6ceb751d6
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-11 17:20:58 -0700

    added constants and refactored a little bit

--- client.js
+++ client.js
@@ -1,10 +1,15 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
+
+
+console.log(IP);
+console.log(PORT);
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
+    host: IP,
+    port: PORT
   }
   );
 
--- input.js
+++ input.js
@@ -2,24 +2,6 @@
 
 let connection;
 
-// const handleUserInput = function(key) {
-//   if (key === '\u0077') {
-//     connection.write('Move: up')
-//   }
-//   if (key === '\u0061') {
-//     connection.write('Move: left')
-//   }
-//   if (key === '\u0073') {
-//     connection.write('Move: down')
-//   }
-//   if (key === '\u0064') {
-//     connection.write('Move: right')
-//   }
-//   if (key === '\u0003') {
-//     process.exit();
-//   }
-// };
-
 
 const setupInput = function(conn) {
   connection = conn;
@@ -27,31 +9,37 @@ const setupInput = function(conn) {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  const handleUserInput = function(key) {
-    if (key === '\u0077') {
-      connection.write('Move: up');
-    }
-    if (key === '\u0061') {
-      connection.write('Move: left');
-    }
-    if (key === '\u0073') {
-      connection.write('Move: down');
-    }
-    if (key === '\u0064') {
-      connection.write('Move: right');
-    }
-    if (key === '\u0072') {
-      connection.write('Say: I am smol')
-    }
-    if (key === '\u0003') {
-      process.exit();
-    }
-
-  };
   stdin.on("data", handleUserInput);
   return stdin;
 };
 
-setupInput();
+const handleUserInput = function(key) {
+  if (key === '\u0077') {
+    connection.write('Move: up');
+  }
+  if (key === '\u0061') {
+    connection.write('Move: left');
+  }
+  if (key === '\u0073') {
+    connection.write('Move: down');
+  }
+  if (key === '\u0064') {
+    connection.write('Move: right');
+  }
+  if (key === '\u0072') {
+    connection.write('Say: I am smol');
+  }
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+
+
+
+// setupInput();
+
+
 
 module.exports = setupInput;
+
